d87030349e1bad8de2fb43e70a407fe67a6e5d0a
kelsey17 <kelseys@cricketscircle.com>
2016-05-31 23:02:15 -0700

    fixed issues with age variable, added 2011 data

--- adding data.R
+++ adding data.R
@@ -1,6 +1,6 @@
 source('final script.R')
 data_2014 <- select(data, -PAR.3, -PAR.4, -greens.hit, -GIR.RANK, -TOTAL, -AVERAGE,
-                    -TOTAL.PUTTS.GAINED)
+                    -TOTAL.PUTTS.GAINED, -AGE)
 X_2014 <- select(data_2014, -POS)
 
 # uploading data from 2013
@@ -44,7 +44,7 @@ data_2013 <- basic %>%
 data_2013 <- data_2013[!is.na(data_2013$POS),] # only take players who participated in Masters
 y_2013 <- as.vector(data_2013$POS)
 row.names(data_2013) <- data_2013$PLAYER
-data_2013 <- select(data_2013, -PLAYER) # set player names as row names, remove from data frame
+data_2013 <- select(data_2013, -PLAYER, -AGE) # set player names as row names, remove from data frame
 for(i in 1:ncol(data_2013)){ # set missing values to mean of that column
   data_2013[is.na(data_2013[,i]), i] <- mean(data_2013[,i], na.rm = TRUE)
 }
@@ -88,12 +88,10 @@ data_2012 <- basic %>%
   left_join(d15, by = "PLAYER") %>% 
   left_join(results, by = "PLAYER")
 
-data_2012$POS <- as.numeric(data_2012$POS)
-data_2012$AGE <- as.numeric(data_2012$POS)
 data_2012 <- data_2012[!is.na(data_2012$POS),] # only take players who participated in Masters
 y_2012 <- as.vector(data_2012$POS)
 row.names(data_2012) <- data_2012$PLAYER
-data_2012 <- select(data_2012, -PLAYER) # set player names as row names, remove from data frame
+data_2012 <- select(data_2012, -PLAYER, -AGE) # set player names as row names, remove from data frame
 for(i in 1:ncol(data_2012)){ # set missing values to mean of that column
   data_2012[is.na(data_2012[,i]), i] <- mean(data_2012[,i], na.rm = TRUE)
 }
@@ -101,10 +99,58 @@ X_2012 <- select(data_2012, -POS) # matrix without response variable
 
 # adding 2011
 
+basic <- read.csv('Masters Data 2011 - Basic Data 2011.csv')
+d1 <- read.csv('Masters Data 2011 - Birdie Conversions 2011.csv')
+d2 <- read.csv('Masters Data 2011 - Bounce Back 2011.csv')
+d3 <- read.csv('Masters Data 2011 - Club Head Speed 2011.csv')
+d4 <- read.csv('Masters Data 2011 - Going for the Green 20111.csv')
+d5 <- read.csv('Masters Data 2011 - Hit Fairway % 2011.csv')
+d6 <- read.csv('Masters Data 2011 - One Putt % 2011.csv')
+d7 <- read.csv('Masters Data 2011 - Par 5 Scoring Average 2011.csv')
+d8 <- read.csv('Masters Data 2011 - Proximity to the Hole 2011.csv')
+d9 <- read.csv('Masters Data 2011 - Putts Per Round 2011.csv')
+d10 <- read.csv('Masters Data 2011 - Rough Tendency 2011.csv')
+d11 <- read.csv('Masters Data 2011 - Sand Save % 2011.csv')
+d12 <- read.csv('Masters Data 2011 - Scoring Average 2011.csv')
+d13 <- read.csv('Masters Data 2011 - Scrambling 2011.csv')
+d14 <- read.csv('Masters Data 2011 - Strokes Gained Putting 2011.csv')
+d15 <- read.csv('Masters Data 2011 - Strokes Gained Tee to Green 2011.csv')
+results <- read.csv('Masters Data 2011 - RESULTS.csv')
+
+data_2011 <- basic %>% 
+  left_join(d1, by = "PLAYER") %>% 
+  left_join(d2, by = "PLAYER") %>% 
+  left_join(d3, by = "PLAYER") %>% 
+  left_join(d4, by = "PLAYER") %>% 
+  left_join(d5, by = "PLAYER") %>% 
+  left_join(d6, by = "PLAYER") %>% 
+  left_join(d7, by = "PLAYER") %>% 
+  left_join(d8, by = "PLAYER") %>% 
+  left_join(d9, by = "PLAYER") %>% 
+  left_join(d10, by = "PLAYER") %>% 
+  left_join(d11, by = "PLAYER") %>%
+  left_join(d12, by = "PLAYER") %>% 
+  left_join(d13, by = "PLAYER") %>% 
+  left_join(d14, by = "PLAYER") %>% 
+  left_join(d15, by = "PLAYER") %>% 
+  left_join(results, by = "PLAYER")
+
+data_2011 <- data_2011[!is.na(data_2011$POS),] # only take players who participated in Masters
+y_2011 <- as.vector(data_2011$POS)
+row.names(data_2011) <- data_2011$PLAYER
+data_2011 <- select(data_2011, -PLAYER, -AGE) # set player names as row names, remove from data frame
+for(i in 1:ncol(data_2011)){ # set missing values to mean of that column
+  data_2011[is.na(data_2011[,i]), i] <- mean(data_2011[,i], na.rm = TRUE)
+}
+X_2011 <- select(data_2011, -POS) # matrix without response variable
+
+# add data from 2010
+
+
 
 
 
 # join all sets
 
-full <- rbind(X_2014, X_2013, X_2012)
+full <- rbind(X_2014, X_2013, X_2012, X_2011)
 
